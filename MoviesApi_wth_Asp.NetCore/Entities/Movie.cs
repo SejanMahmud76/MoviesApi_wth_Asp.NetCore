a1f1af30cd49e7fa5dc4f957df890edecc1a6c95
Rakibul-Hasan-Sezan <sejan16@cse.pstu.ac.bd>
2025-07-20 12:49:18 +0600

    step3: Wrote code to establish relationship between 2 properties

--- MoviesApi_wth_Asp.NetCore/Entities/Movie.cs
+++ MoviesApi_wth_Asp.NetCore/Entities/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace MoviesApi_wth_Asp.NetCore.Entities
 {
@@ -14,7 +15,12 @@ namespace MoviesApi_wth_Asp.NetCore.Entities
         [Range(1,180)]
         public decimal Price {  get; set; }
 
+        //Navigation property
+        [ValidateNever]
+        public Genre? Genre { get; set; }   
 
+        //Foreign Key property
+        public int GenreId { get; set; }
         public DateOnly ReleseDate { get; set; }
     }
 }
