2d7f4b26a456621f300e1aaf6a612993824a1c80
Rakibul-Hasan-Sezan <sejan16@cse.pstu.ac.bd>
2025-07-20 14:16:57 +0600

    Use Connection string from Apsetting.json

--- MoviesApi_wth_Asp.NetCore/Program.cs
+++ MoviesApi_wth_Asp.NetCore/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace MoviesApi_wth_Asp.NetCore
 {
     public class Program
@@ -6,6 +8,13 @@ namespace MoviesApi_wth_Asp.NetCore
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
+
+
+            //Use Connection string from Apsetting.json
+            builder.Services.AddDbContext<MovieContext>(optionsn =>
+
+                optionsn.UseSqlServer(builder.Configuration.GetConnectionString("MovieDbCS"))
+            );    
 
             app.MapGet("/", () => "Hello World!");
 
